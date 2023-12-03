2adf66e5e4cf66091892c126efd2a236280a2e96
lmoreau2021 <lmoreau2021@gmail.com>
2023-12-02 21:32:02 -0600

    nav?

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import DailyChallenge from "./pages/DailyChallenge";
 import EndlessMode from "./pages/EndlessMode";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/Polygloter" element={<Home />} />
-      <Route path="/Polygloter/daily-challenge" element={<DailyChallenge />} />
-      <Route path="/Polygloter/endless-mode" element={<EndlessMode />} />
+    <HashRouter basename="/Polygloter">
+      <Route path="/" element={<Home />} />
+      <Route path="/daily-challenge" element={<DailyChallenge />} />
+      <Route path="/endless-mode" element={<EndlessMode />} />
       <Route path="*" element={<Home />} /> {/* Fallback for 404 */}
-    </Routes>
+    </HashRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import "./custom-styles.css"
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 const container = document.getElementById("root");
 const root = createRoot(container);
 import { ChakraProvider } from '@chakra-ui/react';
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
    <ChakraProvider>
       <App />
     </ChakraProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
